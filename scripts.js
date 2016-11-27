671d1e11c509ac09fcb6061adec2fefe22df90a4
Keller Flint <kflint0068@gmail.com>
2016-11-27 14:42:38 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 function getName() {
     // Test Data
-    var nouns = ["Bicycles", "Juice", "Babies", "Scarves", "Souls"]
-    var adjectives = ["Organic", "Authentic", "Vintage", "Natural"]
+    var firstNames = ["Cycle", "Juice", "Babies"]
+    var lastNames = ["Organic", "Soul", "Authentic"]
 
     // Combines first and last names
-    document.getElementById('store').innerHTML = adjectives[Math.floor(Math.random() * adjectives.length)] + " " + nouns[Math.floor(Math.random() * nouns.length)];
+    document.getElementById('store').innerHTML = firstNames[Math.floor(Math.random() * firstNames.length)] + " " + lastNames[Math.floor(Math.random() * lastNames.length)];
 }
