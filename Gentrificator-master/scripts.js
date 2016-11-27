3a34bb5a00ebff89b263b2b4b71c8181c81d1be1
Keller Flint <kflint0068@gmail.com>
2016-11-27 14:37:16 -0600

    Delete scripts.js

--- Gentrificator-master/scripts.js
+++
@@ -1,8 +0,0 @@
-function getName() {
-    // Test Data
-    var nouns = ["Bicycles", "Juice", "Babies", "Scarves", "Souls"]
-    var adjectives = ["Organic", "Authentic", "Vintage", "Natural"]
-
-    // Combines first and last names
-    document.getElementById('store').innerHTML = adjectives[Math.floor(Math.random() * nouns.length)] + " " + lastNames[Math.floor(Math.random() * lastNames.length)];
-}
